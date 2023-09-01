bc37f7f4d9a9cc26f4607060df356bdc4dd2bd4b
Wyatt <wyattash@umich.edu>
2023-08-31 23:22:58 -0400

    Added Experiance Tab and Images

--- app.js
+++ app.js
@@ -11,3 +11,25 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+function open_description_click(clickid) {
+    document.getElementById(clickid + "_description").style.display = "flex";
+    document.getElementById(clickid + "_description").style.opacity = "100%";
+
+    full_container = document.getElementById("experiance-fullscreen-container");
+    full_container.style.display = "flex";
+    full_container.style.opacity = "100%";
+
+    click_handler = function() {
+        document.getElementById(clickid + "_description").style.opacity = "0%";
+
+        full_container.style.opacity = "0%";
+        setTimeout(function() {
+            full_container.style.display = "none";
+        });
+
+        full_container.removeEventListener("click", this);
+    };
+
+    full_container.addEventListener("click", click_handler);
+}
